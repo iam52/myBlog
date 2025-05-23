a001b08ff17b319755e4bf138abbf7de84ec5faf
iam52 <tupac52@gmail.com>
2025-05-23 20:03:40 +0900

    validation settings

--- src/main/java/me/superoi/springbootdeveloper/dto/AddArticleRequest.java
+++ src/main/java/me/superoi/springbootdeveloper/dto/AddArticleRequest.java
@@ -1,5 +1,7 @@
 package me.superoi.springbootdeveloper.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,7 +12,11 @@ import me.superoi.springbootdeveloper.domain.Article;
 @Getter
 public class AddArticleRequest {
 
+    @NotNull
+    @Size(min = 1, max = 10)
     private String title;
+
+    @NotNull
     private String content;
 
     public Article toEntity(String author) { // 생성자를 사용해 객체 생성
