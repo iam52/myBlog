14c9e4daf7919518fc72e8f2c262950a8ab0a104
iam52 <tupac52@gmail.com>
2025-05-23 14:58:03 +0900

    ComponentScan setting

--- src/main/java/me/superoi/springbootdeveloper/SpringbootDeveloperApplication.java
+++ src/main/java/me/superoi/springbootdeveloper/SpringbootDeveloperApplication.java
@@ -2,10 +2,12 @@ package me.superoi.springbootdeveloper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing // created_at, updated_at 자동 업데이트
+@ComponentScan(basePackages = "me.superoi.springbootdeveloper")
 public class SpringbootDeveloperApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootDeveloperApplication.class, args);
