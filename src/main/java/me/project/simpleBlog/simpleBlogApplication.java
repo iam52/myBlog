54045485b207d912c39b2ada6ffe5793893d8a40
iam52 <tupac52@gmail.com>
2025-09-02 22:47:49 +0900

    initial commit

---
+++ src/main/java/me/project/simpleBlog/simpleBlogApplication.java
@@ -0,0 +1,11 @@
+package me.project.simpleBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class simpleBlogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(simpleBlogApplication.class, args);
+    }
+}
