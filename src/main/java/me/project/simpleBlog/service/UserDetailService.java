221f40da04016ee2636702f9a7a0ab3eb6e96abb
iam52 <tupac52@gmail.com>
2025-09-16 21:49:23 +0900

    User 레포지토리, 서비스 구현

---
+++ src/main/java/me/project/simpleBlog/service/UserDetailService.java
@@ -0,0 +1,19 @@
+package me.project.simpleBlog.service;
+
+import lombok.RequiredArgsConstructor;
+import me.project.simpleBlog.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) {
+        return userRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException(email));
+    }
+}
